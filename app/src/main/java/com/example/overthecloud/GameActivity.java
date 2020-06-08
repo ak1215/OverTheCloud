6b5fd87e47096dcc320083d48dd1767883823320
ak1215 <60421957+ak1215@users.noreply.github.com>
2020-06-08 11:16:03 +0200

    The start of Game Activity...

---
+++ app/src/main/java/com/example/overthecloud/GameActivity.java
@@ -0,0 +1,13 @@
+package com.example.overthecloud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
